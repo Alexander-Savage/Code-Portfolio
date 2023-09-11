96aa802defdffb3f6d9b48c824878c69e0dbd5d4
Alex Savage <asavag19@ford.com>
2023-09-11 09:37:29 +0100

    recent changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import {React, useState, useEffect, useRef} from 'react'
 import './App.css';
 import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Switch, Route, Link, Routes } from 'react-router-dom';
@@ -6,14 +7,46 @@ import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import Contact from './components/Contact';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';  
+import Button from '@mui/material/Button';  
+import axios from 'axios'; 
+import axiosInstance from './config/axiosInstance';
+
+const item = {
+  name: 'Alex Savage',
+  description: 'This is a test item',
+  price: 9.99,
+  tax: 0.99
+};
+
+
+
 
 
 function App() {
+  const [content, setContent] = useState('This is the content of the div');
+  const div = useRef(null);
+
+  function callApi() {
+    axiosInstance.get('/api')
+    .then(response => {
+    setContent(response.data.message);
+      console.log(response.data.message);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+
+  // () => useEffect({
+  //   callApi()
+  // }, [])
+
   return (
     <div className="App">
       <Router>
       <NavBar/> 
-      <Container maxWidth="100vw">
+      <Container ref={div} maxWidth="100vw">
       <header className="App-header" maxWidth ="100vw">
         <Routes >
           <Route path="/" element={<HomePage />} />
@@ -23,6 +56,9 @@ function App() {
           <Route path="/contact" element={<Contact />}/>
 
         </Routes>
+        <div>{content}</div>
+          <Button variant="contained" onClick={callApi}>Contained</Button>  
+
         </header>
       </Container>
       </Router>
