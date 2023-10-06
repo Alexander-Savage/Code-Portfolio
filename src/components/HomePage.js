9b1fbc072149bb3ded9e32d3d35aad55d5badb7c
Alex Savage <asavag19@ford.com>
2023-10-06 21:46:19 +0100

    updating front page before posting on instagram

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,11 +10,13 @@ function HomePage() {
       This is my Coding portfolio where I can show off my skills to learn new languages quickly.
       <br/>
       This also shows my ability to develop a full stack website and deploy it!
+
+      Come and learn with me on my instagram page <a href="https://instagram.com/savage.software?igshid=NzZlODBkYWE4Ng==">@savage.software!!</a>
       </p>
-      <div className='card'>
+      {/* <div className='card'>
         <div>Some cool text</div>
-      </div>
-      <div style={{height: '100vh'}}></div>
+      </div> */}
+      {/* <div style={{height: '100vh'}}></div> */}
     </div>
   );
 }
