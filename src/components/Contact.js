a13fbb3d5cbeee88a255cabcb75f7baa6fb2f245
Alex Savage <asavag19@ford.com>
2023-10-06 21:56:55 +0100

    Update contact page to use logo's of instagram and linkedIn

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,18 @@ function Contact() {
     <div>
       <h1>Contact Page</h1>
       
-      <a href="https://www.linkedin.com/in/alex-savage-3776a2216/">Linkedin!</a>
+      <a href="https://www.linkedin.com/in/alex-savage-3776a2216/" target="_blank">
+          <img src="src/images/LinkedIn_icon.svg.png" alt="LinkedIn Profile" />
+      </a>
 
-      <a href="https://instagram.com/savage.software?igshid=NzZlODBkYWE4Ng==">Instagram!</a>
+      <a href="https://www.instagram.com/savage.software/?igshid=NzZlODBkYWE4Ng%3D%3D" target="_blank">
+          <img src="../images/src/images/Instagram_logo_2016webp.webp" alt="LinkedIn Profile" />
+      </a>
+
+
+      {/* <a href="https://www.linkedin.com/in/alex-savage-3776a2216/">Linkedin!</a>
+
+      <a href="https://instagram.com/savage.software?igshid=NzZlODBkYWE4Ng==">Instagram!</a> */}
       
 
     </div>
