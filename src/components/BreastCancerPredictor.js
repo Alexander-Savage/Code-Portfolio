81312bae29967a9568a2a3f259543f3a6dcecc3d
Alex Savage <asavag19@ford.com>
2024-06-18 21:28:25 +0100

    Update href links security vulnerabilities.

--- src/components/BreastCancerPredictor.js
+++ src/components/BreastCancerPredictor.js
@@ -5,7 +5,7 @@ import python from '../images/python-code.jpg'
 function BreastCancerPredictor() {
   return (
     <div>
-      <a href="#" title="python-code">
+      <a href="#/o" title="python-code">
       <h1>Here's a simple a.i. python predictor</h1>
       <p>Credit to @Khanradcoder for the tutorial!</p>
         <img classname="python-image" alt="python code" src={python} height="500" width="700" />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,11 +12,11 @@ function Contact() {
           <link rel="stylesheet" type="text/css" href="styles.css"></link>
       </head> */}
 
-      <a className='linkedin-logo'  href="https://www.linkedin.com/in/alex-savage-3776a2216/" target="_blank" >
+      <a className='linkedin-logo'  href="https://www.linkedin.com/in/alex-savage-3776a2216/" rel="noopener noreferrer" target="_blank" >
           <img src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg" alt="LinkedIn Profile" height="100" width="100"/>
       </a>
 
-      <a className='instagram-logo'  href="https://www.instagram.com/savage.software/?igshid=NzZlODBkYWE4Ng%3D%3D" target="_blank">
+      <a className='instagram-logo'  href="https://www.instagram.com/savage.software/?igshid=NzZlODBkYWE4Ng%3D%3D" rel="noopener noreferrer" target="_blank">
           <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" alt="Instagram Profile" height="100" width="100" />
       </a>
 
